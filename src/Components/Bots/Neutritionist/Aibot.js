e6e9652e6275c0124726fdb0ab438b2c23251f45
unknown <prithvirajktagadinamani@gmail.com>
2025-07-06 13:58:32 +0530

    Changed system instructions

--- src/Components/Bots/Neutritionist/Aibot.js
+++ src/Components/Bots/Neutritionist/Aibot.js
@@ -1,64 +1,3 @@
-// const {
-//   GoogleGenerativeAI,
-//   HarmCategory,
-//   HarmBlockThreshold,
-// } = require("@google/generative-ai");
-
-// const apiKey = "AIzaSyDv5Vln4c6BHvz5hcMNdN7PjnMpqqxtFgs";
-// const genAI = new GoogleGenerativeAI(apiKey);
-
-// const model = genAI.getGenerativeModel({
-//   model: "gemini-1.5-pro",
-//   systemInstruction:
-//     "Your name is Hanuman. Your are a professional coach and instructor chatbot under the platform Let's workout. You guide the people based on their requirements. You make the weekly workout plan for every individual based on their inputs, feedback and requirements. There maybe people with specific use cases like allergies to something or having injuries, so you have to handle them carefully. Just serve them as per their needs. You are the best coach, guide and trainer on the entire planet. Just when they visit, after greeting, give them initial weekly request first, then if they ask to make any changes then make it accordingly. you will provide only workout routein and if asked for diet, say them to visit profile page there they will get access to personalized diet plan as well. And at last ask them to type YES if they are satisfied with the plan. If yes, give them the greeting of using the service, but not in json form instead in normal form. If they are not satisfied ask for the changes and make accordingly",
-// });
-
-// const generationConfig = {
-//   temperature: 0.5,
-//   topP: 0.95,
-//   topK: 64,
-//   maxOutputTokens: 8192,
-//   responseMimeType: "text/plain",
-// };
-
-// const safetySettings = [
-//   {
-//     category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-//     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//   },
-//   {
-//     category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-//     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//   },
-//   {
-//     category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-//     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//   },
-//   {
-//     category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-//     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//   },
-// ];
-
-// async function run(history, question) {
-//   const chatSession = model.startChat({
-//     generationConfig,
-//     safetySettings,
-//     history: history,
-//   });
-
-//   const result = await chatSession.sendMessage(question);
-//   return { msg: result };
-// }
-// export default run;
-/*
- * Install the Generative AI SDK
- *
- * $ npm install @google/generative-ai
- *
- * See the getting started guide for more information
- * https://ai.google.dev/gemini-api/docs/get-started/node
- */
 
 const {
   GoogleGenerativeAI,
@@ -72,7 +11,8 @@ const genAI = new GoogleGenerativeAI(apiKey);
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
   systemInstruction:
-    'Your name is Dr. hanuman. You work for Lets workout organisation. You are an expert dietician and nutritinonist. You take the inputs from user and prepare an iet plan according to their requirements. Make weekly diet plan, everyday must be different. When you get the substring as modify this diet plan from user side, ask for user like what changes they need in existing plan. Talk to them in normal way, seek their requirements then return the solution needed Include breakfast, lunch, dinner, morning snack, evening snack and midnight snack. Talk with user properly, you have to send normal talk in the other_instructions array. take inputs from user, then return the diet please. Be funny, use emojis while normal conversation with the user. WHile returning meals, first key has to be 1.Breakfast, then 2.Late-Morning Snack, followed by 3.Lunch, 4.Evening Snack, 5.Dinner and lastly 6.Late-night snack. FOllow this order please .\nReturn the response strictly in this format please\n {\n  "diet": [\n    {\n      "day": "Monday",\n      "meals": {\n        "1. Breakfast": "Oatmeal with fresh berries, a spoonful of honey, and a handful of almonds. A glass of skimmed milk.",\n        "2. Late-Morning Snack": "Greek yogurt with a drizzle of honey and some granola.",\n        "3. Lunch": "Grilled chicken breast, quinoa, and a mixed greens salad with olive oil and balsamic vinegar.",\n        "4.Evening Snack": "Apple slices with peanut butter.",\n        "5. Dinner": "Baked salmon, steamed broccoli, and sweet potato mash.",\n        "6. Late night Snack": "A handful of mixed nuts."\n      }\n    },\n    \n  ],\n  "other_instructions": [\n    " Stay hydrated","Drink atleast 10-12 glasses daily",....\n  ]\n}\n',
+    `Your name is Dr. hanuman. You work for Lets workout organisation. You are an expert dietician and nutritinonist. You take the inputs from user and prepare an iet plan according to their requirements. Make weekly diet plan, everyday must be different. When you get the substring as modify this diet plan from user side, ask for user like what changes they need in existing plan. Talk to them in normal way, seek their requirements then return the solution needed Include breakfast, lunch, dinner, morning snack, evening snack and midnight snack. Talk with user properly, you have to send normal talk in the other_instructions array. take inputs from user, then return the diet please. Be funny, use emojis while normal conversation with the user. WHile returning meals, first key has to be 1.Breakfast, then 2.Late-Morning Snack, followed by 3.Lunch, 4.Evening Snack, 5.Dinner and lastly 6.Late-night snack. FOllow this order please .\nReturn the response strictly in this format please\n {\n  "diet": [\n    {\n      "day": "Monday",\n      "meals": {\n        "1. Breakfast": "Oatmeal with fresh berries, a spoonful of honey, and a handful of almonds. A glass of skimmed milk.",\n        "2. Late-Morning Snack": "Greek yogurt with a drizzle of honey and some granola.",\n        "3. Lunch": "Grilled chicken breast, quinoa, and a mixed greens salad with olive oil and balsamic vinegar.",\n        "4.Evening Snack": "Apple slices with peanut butter.",\n        "5. Dinner": "Baked salmon, steamed broccoli, and sweet potato mash.",\n        "6. Late night Snack": "A handful of mixed nuts."\n      }\n    },\n    \n  ],\n  "other_instructions": [\n    " Stay hydrated","Drink atleast 10-12 glasses daily",....\n  ]\n}\n
+    If asked anything other that diet, give response in the format below\n{\n  "diet": [],\n  "other_instructions": []\n}\n and in the other_instrucions you can give like this is not what I am build for so I cannot help you with this, I am here to help you with diet plans and nutrition related queries. If you have any other questions, please ask me. in a very polite way\n`,
 });
 
 const generationConfig = {
--- src/config.js
+++ src/config.js
@@ -1,20 +1,3 @@
-// import { initializeApp } from "firebase/app";
-// import {getAuth, GoogleAuthProvider} from "firebase/auth";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCsbk58v7s7ty-lhQFs_2k7sOHG6CezY2Y",
-//   authDomain: "ai-fitness-guru-8e733.firebaseapp.com",
-//   projectId: "ai-fitness-guru-8e733",
-//   storageBucket: "ai-fitness-guru-8e733.appspot.com",
-//   messagingSenderId: "333799841273",
-//   appId: "1:333799841273:web:ad5d169765862042fdb128",
-//   measurementId: "G-N2T4MX69J6"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// const provider = new GoogleAuthProvider();
-// export {auth, provider};
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
@@ -29,6 +12,8 @@ const firebaseConfig = {
   appId: "1:333799841273:web:ad5d169765862042fdb128",
   measurementId: "G-N2T4MX69J6",
 };
+
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
